e6953f0ae4532fb5888669d4e6686b1038f7cdbc
fazel87 <71261937+fazel87@users.noreply.github.com>
2021-09-02 13:28:49 +0430

    Add files via upload

---
+++ VS_BackupTools/PathConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_BackupTools
+{
+    static class PathConverter
+    {
+        public static string BackSlashPath2SlashPath(this string path)
+        {
+            return path.Replace('\\', '/');
+        }
+    }
+}
---
+++ VS_BackupTools/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace VS_BackupTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime thisDay = DateTime.Now;
+            if (args.Length >= 3)
+            {
+                if (Directory.Exists(args[0].BackSlashPath2SlashPath()) && !File.Exists(args[1].BackSlashPath2SlashPath()) && Directory.Exists(Path.GetDirectoryName(args[1].BackSlashPath2SlashPath())))
+                {
+                    ZipFile.CreateFromDirectory(args[0].BackSlashPath2SlashPath(), args[1].BackSlashPath2SlashPath() + "-" + args[2] + "-" + thisDay.ToString("yyyy-MM-dd hh.mm.ss") + ".zip");
+                }
+            }
+        }
+    }
+}
